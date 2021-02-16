091f1084fbd1145df32b100921f5262076eadff7
Amanuel <weditedi25@gmail.com>
2021-02-16 11:27:20 +0000

    level 100 finished

--- script.js
+++ script.js
@@ -6,7 +6,40 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList[0].name} episodes(s)`;
+
+  for( let i = 0; i < episodeList.length; i++){
+    let episodeContainer = document.createElement("div");
+    episodeContainer.className = "episodeContainer";
+    let episodeName = document.createElement("h2");
+    let episodeImg = document.createElement("img");
+    let episodeSummary = document.createElement("p");
+    
+    //giving values to the elements
+    if(episodeList[i].number < 10){
+      episodeName.innerHTML = episodeList[i].name + " " + "-" + " " + "S0" + episodeList[i].season + "E0" + episodeList[i].number;
+    }
+    else{
+      episodeName.innerHTML = episodeList[i].name + " " + "-" + " " + "S0" + episodeList[i].season + "E" + episodeList[i].number;
+    }
+    
+    episodeImg.src = episodeList[i].image.medium;
+    episodeSummary.innerHTML = episodeList[i].summary;
+
+    // appending elements of each episode
+    episodeContainer.appendChild(episodeName);
+    episodeContainer.appendChild(episodeImg);
+    episodeContainer.appendChild(episodeSummary);
+    // episodeContainer.append(episodeList[i].summary);
+
+    rootElem.appendChild(episodeContainer);
+    console.log(episodeContainer);
+  }
+  
+  
+
+
+
 }
 
 window.onload = setup;
